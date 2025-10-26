abb9e93199d08b336504b3659ed0953d27189afc
Yassine_elKhamlichi <yassinelkhamlichi98@gmail.com>
2025-10-26 14:37:07 +0100

    Some changes
    Updated database credentials to use placeholders for security.

--- Cabinet_Doctor_With_Teminal/Gestion CabinetDoctor/src/gestion/cabinetdoctor/Controles/BDInfo.java
+++ Cabinet_Doctor_With_Teminal/Gestion CabinetDoctor/src/gestion/cabinetdoctor/Controles/BDInfo.java
@@ -2,8 +2,8 @@ package gestion.cabinetdoctor.Controles;
 public class BDInfo {
 	protected static String bdName = "cabinetdoctor";
 	protected static String protocol = "jdbc:mysql:";
-	protected static String user = "root";
-	protected static String password = "9999";
+	protected static String user = "your username";
+	protected static String password = "****";
 	protected static String ip = "127.0.0.1";
 	protected static int port = 3306;
 	protected static String url = protocol + "//" + ip + ":" + port + "/" + bdName+"?useSSL=false&serverTimezone=UTC";
